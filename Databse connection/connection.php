3fe7c482379892fe4ac38312a6fb100f40837d32
Maha-bub <mdmahabubulalam0511@gmaile.com>
2026-04-18 12:19:59 +0600

    connect successfully

--- Databse connection/connection.php
+++ Databse connection/connection.php
@@ -3,9 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Connection mySQL to the database</title>
 </head>
 <body>
-    
+    <h2>Connecting local server :</h2>
 </body>
 </html>
+
+<?php
+$host="localhost";
+$user="root";
+
+$pass="";
+$database="pwad_batch-70";
+
+$conn=new mysqli($host,$user,$pass,$database);
+
+if($conn->connect_error){
+    die("Connection Failed.".$conn->connect_error);
+}echo "Connection successfully!";
+?>
