4c8cb0bcc9f8b69979e0717a5a793f24c21c68d6
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-04-26 11:44:05 +0600

    Insert into Information table from input form

--- insert.php
+++ insert.php
@@ -17,7 +17,57 @@ if(isset($_POST['submit'])){
 }
 
 ?>
+<?php
+$db=new mysqli("localhost","root","","mahabub_70");
+if(isset($_POST['submit'])){
+    $id=$_POST['id'];
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $contact=$_POST['contact'];
 
+    $tabledata="INSERT INTO Information (id,name,email,contact) values('$id','$name','$email','$contact')";
+    if(mysqli_query($db,$tabledata)==true){
+        echo "Data input Success!";
+        header("location:table.php");
+    }
+    else{
+        "Data not inserted". mysqli_error($db);
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table show_source</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <form action="" method="POST" class="mt-4">
+                    <div class="mb-3">
+                        <input type="text" name="id" class="form-control" placeholder="ID" required>
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" name="name" class="form-control" placeholder="Name" required>
+                    </div>
+                    <div class="mb-3">
+                        <input type="email" name="email" class="form-control" placeholder="Email" required>
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" name="contact" class="form-control" placeholder="Contact" required>
+                    </div>
+                    <button type="submit" name="submit" class="btn btn-primary w-100">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
 <!DOCTYPE html>
 <html lang="en">
 <head>
